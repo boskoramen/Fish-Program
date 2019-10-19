5ffd87cddadf1a9b9f32a7930c02d735f37ca0c0
lother <thedonkified.lotherjones@gmail.com>
2019-10-19 15:39:13 -0400

    Creates Player class

---
+++ Player.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Player {
+    private int unknown_cards = 0;
+    private ArrayList<Card> known_cards = ArrayList();
+}
+
+public int get_unknown_cards() {
+    return unknown_cards;
+}
+
+public ArrayList<Card> get_known_cards() {
+    return known_cards;
+}
+
+public void set_unknown_cards(int offset) {
+    unknown_cards += offset;
+}
+
+public void add_known_cards(Card card) {
+    known_cards.add(card);
+    return;
+}
+
+public boolean remove_known_cards(Card card) {
+    return known_cards.remove(card);
+}
